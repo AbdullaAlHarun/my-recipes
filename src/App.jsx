095404cb751b8d52a8202aa0bfba594de8f67c22
Abdulla Al Harun <abdhar01276@stud.noroff.no>
2024-03-15 23:49:08 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import './index.css'
+
+function App() {
+
+
+  return (
+    <>
+     
+      <h1 className='text-primary underline'>My Recipes</h1>
+     
+      
+    </>
+  )
+}
+
+export default App
